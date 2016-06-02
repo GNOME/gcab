1269f825339916895d67571b23190ff3b6cb03ca
Emmanuele Bassi <ebassi@gnome.org>
2016-06-02 15:16:23 +0100

    build: Ensure that autogen.sh supports builddir != srcdir
    The tools that modify the build system files need to, sadly, do that
    inside the srcdir.
    
    This change is necessary because jhbuild recently switched the default
    to build in a separate directory.

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,18 @@ set -e # exit on errors
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
+
+olddir=`pwd`
+
 mkdir -p "$srcdir/m4"
+
+cd "$srcdir"
 
 gtkdocize
 autoreconf -v --force --install
 intltoolize -f
+
+cd "$olddir"
 
 if [ -z "$NOCONFIGURE" ]; then
     "$srcdir"/configure --enable-gtk-doc "$@"
