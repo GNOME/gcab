e4b1eb21cbb993b73ed2d6abd3bf4e81e33356f3
Richard Hughes <richard@hughsie.com>
2017-12-12 14:32:58 +0000

    trivial: Ensure all source files have a license and copyright statement

--- libgcab/gcab-file.h
+++ libgcab/gcab-file.h
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301 USA
  */
+
 #ifndef _GCAB_FILE_H_
 #define _GCAB_FILE_H_
 
--- libgcab/gcab-folder.h
+++ libgcab/gcab-folder.h
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301 USA
  */
+
 #ifndef _GCAB_FOLDER_H_
 #define _GCAB_FOLDER_H_
 
